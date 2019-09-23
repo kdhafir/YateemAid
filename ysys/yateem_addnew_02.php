6289a9a87079e3879e485b6311c7cafa73c9464f
Khalid Dhafir <eng-khalid@outlook.com>
2019-09-23 12:19:30 +0300

    update

--- ysys/yateem_addnew_02.php
+++ ysys/yateem_addnew_02.php
@@ -6,7 +6,7 @@ Aytam Aid System - Open Source
 require_once '../users/init.php';
 if (!securePage($_SERVER['PHP_SELF'])){die();}
 require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
-
+/*
 // Processing Form
 if(!empty($_POST)){
     $response = preProcessForm();
@@ -15,6 +15,53 @@ if(!empty($_POST)){
     //$goNext = $response['fields']['continueData'];
     postProcessForm($response);
     }
+}*/
+if(!empty($_POST)){
+    uploadYateemDocs();
+}
+function uploadYateemDocs(){
+$target_dir = "aytamattachments/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
 }
 $goNext = Input::get('continueData');
 if($goNext){
@@ -51,6 +98,38 @@ if($goNext){
         </tr>
     </table>
     </div>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>الوثيقة</th>
+                <th>ملفها</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>شهادة الميلاد</td>
+            <td>
+                <form class="" method="post" target="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
+                <div class="form-group row">
+                <input type="file" class="form-control-file col" name="birthCert">
+                <input type="submit" class="btn btn-info col" value="رفع">
+                </div>
+                </form>
+            </td>
+        </tr>
+        <tr>
+            <td>شهادة وفاة الأب</td>
+            <td>
+                <form class="" target="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+                <div class="form-group row">
+                <input type="file" class="form-control-file col" name="deathCert">
+                <input type="submit" class="btn btn-info col" value="رفع">
+                </div>
+                </form>
+            </td>
+        </tr>
+        </tbody>
+    </table>
     
 </div>
 </div>
