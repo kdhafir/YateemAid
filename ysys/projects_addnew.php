f4e253ee70c9594b5f026df0e5687b25661a6e15
Khalid Dhafir <eng-khalid@outlook.com>
2019-10-08 12:35:06 +0300

    Update projects_addnew.php

--- ysys/projects_addnew.php
+++ ysys/projects_addnew.php
@@ -7,9 +7,6 @@ require_once '../users/init.php';
 if (!securePage($_SERVER['PHP_SELF'])){die();}
 require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
 
-//$aytamQ = $db->query("SELECT * FROM `yy_yateem_main_info` ORDER BY id DESC");
-//$counAytam = $aytamQ->count();
-
 ?>
 
 <div class="row text-right" dir="rtl">
@@ -36,8 +33,6 @@ require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
     </div>
 
 </div>
-
-
 
 <!-- footers -->
 
